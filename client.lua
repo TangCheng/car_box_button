740e15dc06a7ca75f4dfc04c3c037e7702d5db13
TangCheng <ctang@thoughtworks.com>
2016-08-18 20:24:06 +0800

    try another way to send post data

--- client.lua
+++ client.lua
@@ -14,17 +14,26 @@ function post_data()
     end
     local url = 'http://' .. parameter[config.URI] .. '/post'
     print(url)
-    http.post(url,
-        'Content-Type: application/json\r\n',
-        '{"test":"value"}',
-        function(code, data)
-            if (code < 0) then
-                print("HTTP request failed")
-            else
-                print(code, data)
-            end
-        end
-    )
+    --http.post(url,
+        --'Content-Type: application/json\r\n',
+        --'{"test":"value"}',
+        --function(code, data)
+            --if (code < 0) then
+                --print("HTTP request failed")
+            --else
+                --print(code, data)
+            --end
+        --end
+    --)
+    sk = net.createConnection(net.TCP, 0)
+    sk:on("receive", function(sck, c) print(c) end)
+    sk:on("connection", function(sck, c)
+        sck:send("POST / HTTP/1.1\r\nHost: " .. url .. "\r\nContent-Type: application/json\r\n{\"test\":\"value\"}\r\n\r\n")
+    end)
+    local address, port = parameter[URI]:match("([^:]+):([^:]+)")
+    if port == nil then port = 80 end
+    print(address .. port)
+    sk:connect(#port, address)
 end
 
 wifi.setmode(wifi.STATION)
